b670eef6fc952d3a6e7661918f623df1fd96a6dc
thiago2828 <thiagomouraemanuel@gmail.com>
2024-06-20 13:34:11 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
